0bf2c4ddf81cd47b20f3d51831e74ccb6ad7872b
Robert Sasu <sasu.robert@gmail.com>
2020-08-03 19:15:26 +0300

    bugfix max nonce diff check

---
+++ target/rls/debug/deps/attestation-3b1c94076e5d845d.d
@@ -0,0 +1,8 @@
+/home/robert/GolandProjects/sc-attestation-rs/target/rls/debug/deps/attestation-3b1c94076e5d845d.rmeta: src/lib.rs src/user_state.rs
+
+/home/robert/GolandProjects/sc-attestation-rs/target/rls/debug/deps/attestation-3b1c94076e5d845d.d: src/lib.rs src/user_state.rs
+
+src/lib.rs:
+src/user_state.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.2.0

c85ce15ac05de1899d1b8bea2c94b9b450e8e31c
dorin.iancu <dorin.iancu@elrond.com>
2020-11-25 11:29:18 +0200

    forgot to save some changes

--- fuzz/fuzz_targets/fuzz_user.rs
+++ fuzz/fuzz_targets/fuzz_user.rs
@@ -2,19 +2,6 @@
 use libfuzzer_sys::fuzz_target;
 
 use elrond_wasm::elrond_codec::*;
-<<<<<<< HEAD
-use elrond_wasm_debug::*;
-
-use attestation::user::User;
-use elrond_codec::top_de::TopDecode;
-
-fuzz_target!(|data: &[u8]| {
-    if let Ok(decoded) = User::top_decode(&mut &data[..]) {
-        let encoded_clean = decoded.top_encode().unwrap();
-        let decoded_again = User::top_decode(&mut &encoded_clean[..]).unwrap();
-        // assert_eq!(decoded, decoded_again);
-        let encoded_again = decoded_again.top_encode().unwrap();
-=======
 use elrond_wasm::elrond_codec::test_util::*;
 use attestation::User;
 
@@ -24,7 +11,6 @@ fuzz_target!(|data: &[u8]| {
         let decoded_again = check_top_decode::<User>(&encoded_clean[..]);
         assert_eq!(decoded, decoded_again);
         let encoded_again = check_top_encode(&decoded_again);
->>>>>>> master
         assert_eq!(encoded_clean, encoded_again);
     }
 });

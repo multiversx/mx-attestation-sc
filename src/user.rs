77429b3c08f2154c90ad3456fef51dec20aa435a
andrei-marinica <andrei.marinica@elrond.com>
2020-12-23 07:29:29 +0200

    elrond-wasm version 0.10.0, refactoring

--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,9 @@
 use super::ValueState;
-use elrond_wasm::elrond_codec::*;
 use elrond_wasm::{Address, BoxedBytes, H256};
 
-#[derive(Clone, PartialEq, Debug)]
+derive_imports!();
+
+#[derive(TopEncode, TopDecode, TypeAbi, Clone, PartialEq, Debug)]
 pub struct User {
 	pub value_state: ValueState,
 	pub public_info: H256,
@@ -12,95 +13,8 @@ pub struct User {
 	pub nonce: u64,
 }
 
-impl NestedEncode for User {
-	fn dep_encode<O: NestedEncodeOutput>(&self, dest: &mut O) -> Result<(), EncodeError> {
-		self.value_state.dep_encode(dest)?;
-		self.public_info.dep_encode(dest)?;
-		self.private_info.dep_encode(dest)?;
-		self.address.dep_encode(dest)?;
-		self.attester.dep_encode(dest)?;
-		self.nonce.dep_encode(dest)?;
-		Ok(())
-	}
-
-	#[allow(clippy::redundant_clone)]
-	fn dep_encode_or_exit<O: NestedEncodeOutput, ExitCtx: Clone>(
-		&self,
-		dest: &mut O,
-		c: ExitCtx,
-		exit: fn(ExitCtx, EncodeError) -> !,
-	) {
-		self.value_state.dep_encode_or_exit(dest, c.clone(), exit);
-		self.public_info.dep_encode_or_exit(dest, c.clone(), exit);
-		self.private_info.dep_encode_or_exit(dest, c.clone(), exit);
-		self.address.dep_encode_or_exit(dest, c.clone(), exit);
-		self.attester.dep_encode_or_exit(dest, c.clone(), exit);
-		self.nonce.dep_encode_or_exit(dest, c.clone(), exit);
-	}
-}
-
-impl TopEncode for User {
-	#[inline]
-	fn top_encode<O: TopEncodeOutput>(&self, output: O) -> Result<(), EncodeError> {
-		top_encode_from_nested(self, output)
-	}
-
-	#[inline]
-	fn top_encode_or_exit<O: TopEncodeOutput, ExitCtx: Clone>(
-		&self,
-		output: O,
-		c: ExitCtx,
-		exit: fn(ExitCtx, EncodeError) -> !,
-	) {
-		top_encode_from_nested_or_exit(self, output, c, exit);
-	}
-}
-
-impl NestedDecode for User {
-	fn dep_decode<I: NestedDecodeInput>(input: &mut I) -> Result<Self, DecodeError> {
-		Ok(User {
-			value_state: ValueState::dep_decode(input)?,
-			public_info: H256::dep_decode(input)?,
-			private_info: BoxedBytes::dep_decode(input)?,
-			address: Address::dep_decode(input)?,
-			attester: Address::dep_decode(input)?,
-			nonce: u64::dep_decode(input)?,
-		})
-	}
-
-	#[allow(clippy::redundant_clone)]
-	fn dep_decode_or_exit<I: NestedDecodeInput, ExitCtx: Clone>(
-		input: &mut I,
-		c: ExitCtx,
-		exit: fn(ExitCtx, DecodeError) -> !,
-	) -> Self {
-		User {
-			value_state: ValueState::dep_decode_or_exit(input, c.clone(), exit),
-			public_info: H256::dep_decode_or_exit(input, c.clone(), exit),
-			private_info: BoxedBytes::dep_decode_or_exit(input, c.clone(), exit),
-			address: Address::dep_decode_or_exit(input, c.clone(), exit),
-			attester: Address::dep_decode_or_exit(input, c.clone(), exit),
-			nonce: u64::dep_decode_or_exit(input, c.clone(), exit),
-		}
-	}
-}
-
-impl TopDecode for User {
-	fn top_decode<I: TopDecodeInput>(input: I) -> Result<Self, DecodeError> {
-		top_decode_from_nested(input)
-	}
-
-	fn top_decode_or_exit<I: TopDecodeInput, ExitCtx: Clone>(
-		input: I,
-		c: ExitCtx,
-		exit: fn(ExitCtx, DecodeError) -> !,
-	) -> Self {
-		top_decode_from_nested_or_exit(input, c, exit)
-	}
-}
-
 #[cfg(test)]
-mod h256_tests {
+mod codec_tests {
 	use super::*;
 	use elrond_wasm::elrond_codec::test_util::{check_top_decode, check_top_encode};
 
--- src/value_state.rs
+++ src/value_state.rs
@@ -1,6 +1,6 @@
-use elrond_wasm::elrond_codec::*;
+derive_imports!();
 
-#[derive(Clone, PartialEq, Debug)]
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi, Clone, PartialEq, Debug)]
 pub enum ValueState {
 	None,
 	Requested,
@@ -10,101 +10,6 @@ pub enum ValueState {
 
 impl ValueState {
 	pub fn exists(&self) -> bool {
-		if let ValueState::None = self {
-			false
-		} else {
-			true
-		}
-	}
-
-	pub fn to_u8(&self) -> u8 {
-		match self {
-			ValueState::None => 0,
-			ValueState::Requested => 1,
-			ValueState::Pending => 2,
-			ValueState::Approved => 3,
-		}
-	}
-}
-
-impl NestedEncode for ValueState {
-	fn dep_encode<O: NestedEncodeOutput>(&self, dest: &mut O) -> Result<(), EncodeError> {
-		self.to_u8().dep_encode(dest)
-	}
-
-	fn dep_encode_or_exit<O: NestedEncodeOutput, ExitCtx: Clone>(
-		&self,
-		dest: &mut O,
-		c: ExitCtx,
-		exit: fn(ExitCtx, EncodeError) -> !,
-	) {
-		self.to_u8().dep_encode_or_exit(dest, c, exit);
-	}
-}
-
-impl TopEncode for ValueState {
-	fn top_encode<O: TopEncodeOutput>(&self, output: O) -> Result<(), EncodeError> {
-		self.to_u8().top_encode(output)
-	}
-
-	fn top_encode_or_exit<O: TopEncodeOutput, ExitCtx: Clone>(
-		&self,
-		output: O,
-		c: ExitCtx,
-		exit: fn(ExitCtx, EncodeError) -> !,
-	) {
-		self.to_u8().top_encode_or_exit(output, c, exit);
-	}
-}
-
-impl NestedDecode for ValueState {
-	fn dep_decode<I: NestedDecodeInput>(input: &mut I) -> Result<Self, DecodeError> {
-		match u8::dep_decode(input)? {
-			0 => Ok(ValueState::None),
-			1 => Ok(ValueState::Requested),
-			2 => Ok(ValueState::Pending),
-			3 => Ok(ValueState::Approved),
-			_ => Err(DecodeError::INVALID_VALUE),
-		}
-	}
-
-	fn dep_decode_or_exit<I: NestedDecodeInput, ExitCtx: Clone>(
-		input: &mut I,
-		c: ExitCtx,
-		exit: fn(ExitCtx, DecodeError) -> !,
-	) -> Self {
-		match u8::dep_decode_or_exit(input, c.clone(), exit) {
-			0 => ValueState::None,
-			1 => ValueState::Requested,
-			2 => ValueState::Pending,
-			3 => ValueState::Approved,
-			_ => exit(c, DecodeError::INVALID_VALUE),
-		}
-	}
-}
-
-impl TopDecode for ValueState {
-	fn top_decode<I: TopDecodeInput>(input: I) -> Result<Self, DecodeError> {
-		match u8::top_decode(input)? {
-			0 => Ok(ValueState::None),
-			1 => Ok(ValueState::Requested),
-			2 => Ok(ValueState::Pending),
-			3 => Ok(ValueState::Approved),
-			_ => Err(DecodeError::INVALID_VALUE),
-		}
-	}
-
-	fn top_decode_or_exit<I: TopDecodeInput, ExitCtx: Clone>(
-		input: I,
-		c: ExitCtx,
-		exit: fn(ExitCtx, DecodeError) -> !,
-	) -> Self {
-		match u8::top_decode_or_exit(input, c.clone(), exit) {
-			0 => ValueState::None,
-			1 => ValueState::Requested,
-			2 => ValueState::Pending,
-			3 => ValueState::Approved,
-			_ => exit(c, DecodeError::INVALID_VALUE),
-		}
+		!matches!(self, ValueState::None)
 	}
 }
